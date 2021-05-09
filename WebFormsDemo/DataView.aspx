17a2757f48189d1003ebd2f104e7caef131f4ff8
Dominik Swatek <dominik.swatek87@gmail.com>
2021-05-09 20:45:13 +0200

    add GridView

---
+++ WebFormsDemo/DataView.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataView.aspx.cs" Inherits="WebFormsDemo.WebForm1" MasterPageFile="~/Site.Master" %>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+     
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridViewDS" runat="server" OnInit="GridViewDS_Init">
+        </asp:GridView>
+        <asp:Button ID="ButtonDs" OnClick="ButtonClick" Text="ClickMe" runat="server"/>
+    </form>
+</asp:Content>
---
+++ WebFormsDemo/DataView.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public List<int> MyList { get; set; } = new List<int>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyList.Add(10);
+            MyList.Add(20);
+            MyList.Add(30);
+
+            GridViewDS.DataSource = MyList;
+            GridViewDS.DataBind();
+        }
+
+        protected void GridViewDS_Init(object sender, EventArgs e)
+        {
+            int i = 1;
+        }
+
+        protected void ButtonClick(object sender, EventArgs e)
+        {
+            Debug.WriteLine("DS_From_Button");
+        }
+    }
+}
